6fa414caac4f1313550e097fe3f3b684d350b277
MusicShare <dlalswo656@nate.com>
2023-05-17 00:54:08 +0900

    2023.05.16 : 댓글 수정, 삭제 스크립트 코드 TodayReplyMore.js로 옮김 해야할 것 : 댓글 더보기에 있는 이전댓글 수정, 삭제 버튼 없는 거 수정 1. admin 계정 회원 리스트 페이징 처리 2. 댓글 수정, 삭제 작성자에게만 보이기(userName 으로 비교하면 될 것 같음), 3. admin 회원 리스트 페이징, 4. layout 사용하여 html 파일 코드 사용량 줄이기

--- src/main/java/MusicShare/MusicShare/user/controller/TodayReplyController.java
+++ src/main/java/MusicShare/MusicShare/user/controller/TodayReplyController.java
@@ -7,9 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
-import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
