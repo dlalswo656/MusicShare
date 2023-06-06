c01622c40dfa633e69e1fb627e9495413ccc8f61
MusicShare <dlalswo656@nate.com>
2023-06-07 00:07:21 +0900

    2023.06.07 : 공지사항 view, Controller 매핑 완료 해야할 것 : 관리자만 글 작성, 수정, 삭제 할 수 있는 게시판 만들기 (Role = admin)으로 하기

---
+++ src/main/java/MusicShare/MusicShare/user/controller/NoticeController.java
@@ -0,0 +1,17 @@
+package MusicShare.MusicShare.user.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+@RequiredArgsConstructor
+public class NoticeController {
+    
+    // 공지사항
+    @GetMapping("/Notice")
+    public String Notice() {
+        return "admin/Notice";
+    }
+}
